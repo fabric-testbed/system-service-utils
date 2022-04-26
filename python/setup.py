6f5db8d58ed968dd3475a0988c918f6e1ed35813
Komal Thareja <kthare10@renci.org>
2022-04-26 14:16:00 -0400

    up python version

--- python/setup.py
+++ python/setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
-  python_requires=">=3.7",
+  python_requires=">=3.9",
   install_requires=requirements,
   setup_requires=requirements,
 )
